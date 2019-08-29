e0c1aae58fc84d36c67f190bdd37d11235d73889
Nathan Loveless <nathansl2003@yahoo.com>
2019-08-28 23:42:14 -0400

    Carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -95,7 +95,6 @@
 // carouselContainer.appendChild(carouselC());
 // console.log(carouselContainer);
 
-<<<<<<< HEAD
 function carouselC() {
   const carousel = document.createElement('div');
   carousel.classList.add('carousel');
@@ -164,10 +163,6 @@ function carouselC() {
   carousel.appendChild(img3);
   carousel.appendChild(img4);
   carousel.appendChild(rightButton);
-=======
-var slideIndex = 1;
-showSlides(slideIndex);
->>>>>>> f23dddbe39fa1ef6b16a9631d0c8584856dc4b07
 
 function plusSlides(n) {
   showSlides((slideIndex += n));
